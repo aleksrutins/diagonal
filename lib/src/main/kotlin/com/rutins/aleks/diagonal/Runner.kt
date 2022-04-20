849c4e43b90a81e507adde2795f9339a1372ecac
Aleks Rūtiņš <aleks@rutins.com>
2022-04-20 10:01:25 -0400

    Implementation!

--- lib/src/main/kotlin/com/rutins/aleks/diagonal/Runner.kt
+++ lib/src/main/kotlin/com/rutins/aleks/diagonal/Runner.kt
@@ -1,8 +1,38 @@
 package com.rutins.aleks.diagonal
 
-fun runAll(tests: Array<(Array<Subject>) -> Test>, subjects: Array<Subject>) {
-    for (test in tests) {
-        val instance = test(subjects)
-        instance.run()
+class Runner(private val tests: Array<TestConstructor<*>>, val logger: Logger) {
+    fun runAll(subjects: Array<Subject>) {
+        logger.logVerbose("Running all tests")
+        for (test in tests) {
+            run(test, subjects)
+        }
     }
+    fun run(test: TestConstructor<*>, subjects: Array<Subject>) {
+        logger.logVerbose("describe " + logger.primaryColor(test.first.simpleName))
+        val instance = test.second(subjects, this)
+        try {
+            instance.run()
+        } catch(err: Throwable) {
+            logger.log(logger.failedColor("FAILED"))
+            logger.log("  ${err.message}")
+        } finally {
+            if (instance.errors.size > 0) {
+                logger.log(logger.failedColor("FAILED in sub-clauses:"))
+                for(err in instance.errors) {
+                    logger.log("  it ${logger.primaryColor(err.first)}")
+                    logger.log("    ${err.second.message}")
+                }
+            }
+        }
+    }
+}
+
+interface Logger {
+    fun log(what: String)
+    fun useColor() = true
+    fun useVerbose() = true
+    fun logVerbose(what: String) = if(useVerbose()) log(what) else Unit
+    fun primaryColor(what: String) = if(useColor()) "\u001b[34m$what\u001b[0m" else what
+    fun successColor(what: String) = if(useColor()) "\u001b[32$what\u001b[0m" else what
+    fun failedColor(what: String) = if(useColor()) "\u001b[31m$what\u001b[0m" else what
 }
--- lib/src/main/kotlin/com/rutins/aleks/diagonal/Test.kt
+++ lib/src/main/kotlin/com/rutins/aleks/diagonal/Test.kt
@@ -1,15 +1,36 @@
 package com.rutins.aleks.diagonal
 
-abstract class Test(val subjects: Array<Subject>) {
+class ExpectationException(message: String) : Exception("Expectation not met: $message")
+typealias TestConstructor<T> = Pair<Class<T>, (Array<Subject>, Runner) -> Test>
+
+abstract class Test(val subjects: Array<Subject>, val runner: Runner) {
     abstract fun run()
+    var errors = mutableListOf<Pair<String, Throwable>>()
     inline fun <reified Needle : Subject> find(): Needle? = subjects.find()
     inline fun <reified Needle : Subject> require(): Needle = subjects.require()
+    fun Any?.expect(message: String) = if(this != null) Unit else throw ExpectationException(message)
+    fun Boolean.expect(message: String) = if(this) Unit else throw ExpectationException(message)
+
+    fun it(doesWhat: String, fn: () -> Unit) {
+        with(runner.logger) {
+            logVerbose("  it ${primaryColor(doesWhat)}")
+        }
+        try {
+            fn()
+        } catch (err: Throwable) {
+            with(runner.logger) {
+                log("  ${failedColor("FAILED")}")
+                log("    ${failedColor("Error:")} ${err.message}")
+            }
+            errors.add(doesWhat to err)
+        }
+    }
 }
 
-inline fun <reified T: Subject> describe(crossinline test: (T) -> Unit): (Array<Subject>) -> Test {
-    return { subjects ->
-        object : Test(subjects) {
-            override fun run() = test(require())
+inline fun <reified T: Subject> describe(crossinline test: Test.(T) -> Unit): TestConstructor<T>{
+    return T::class.java to { subjects, runner ->
+        object : Test(subjects, runner) {
+            override fun run() = this.test(require())
         }
     }
 }
